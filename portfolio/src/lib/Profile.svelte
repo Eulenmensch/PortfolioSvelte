3bc3cc917d820993fdddc41eff72f0a6d727bfb1
Yoshua Woo <yoshua.woo@gmail.com>
2022-06-03 14:46:24 +0200

    first commit

---
+++ portfolio/src/lib/Profile.svelte
@@ -0,0 +1,33 @@
+<div id="personal">
+	<p>Hey there 👋,</p>
+	<p>
+		I'm a driven and ambitious game design graduate from Berlin looking for an entry-level game
+		design position.
+	</p>
+	<p>
+		I focus on game feel and systems. During a 6-month internship at KING Art Games, I was
+		responsible for prototyping and iterating the Combat System and UX of a turn-based tactics game
+		in pre-production.
+	</p>
+</div>
+
+<div id="columns">
+	<div class="profile-column">
+		<h3>I have a knack for</h3>
+		<div class="skill">3Cs Design</div>
+		<div class="skill">Game Feel</div>
+		<div class="skill">Gameplay/Systems</div>
+		<div class="skill">UX Design</div>
+	</div>
+	<div class="profile-column">
+		<div class="skill">Unity / Unreal Engine</div>
+		<div class="skill">Programming / Scripting</div>
+		<div class="skill">Confluence / Wikis / Git</div>
+		<div class="skill">Illustrator / Figma</div>
+	</div>
+	<div class="profile-column">
+		<button class="profile-button">Back to Top</button>
+		<button class="profile-button">About</button>
+		<button class="profile-button">Resumé</button>
+	</div>
+</div>
