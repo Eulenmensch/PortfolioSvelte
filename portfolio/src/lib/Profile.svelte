fb06fcae630e98bd8714f6a297dca359e983dbbc
Yoshua Woo <yoshua.woo@gmail.com>
2022-06-05 18:53:39 +0200

    Split Hero and Profile components into two

--- portfolio/src/lib/Profile.svelte
+++ portfolio/src/lib/Profile.svelte
@@ -1,33 +1,47 @@
-<div id="personal">
-	<p>Hey there 👋,</p>
-	<p>
-		I'm a driven and ambitious game design graduate from Berlin looking for an entry-level game
-		design position.
-	</p>
-	<p>
-		I focus on game feel and systems. During a 6-month internship at KING Art Games, I was
-		responsible for prototyping and iterating the Combat System and UX of a turn-based tactics game
-		in pre-production.
-	</p>
-</div>
+<script lang="ts">
+	import Button from './button.svelte';
+</script>
 
 <div id="columns">
 	<div class="profile-column">
-		<h3>I have a knack for</h3>
+		<h3>I have a knack for:</h3>
 		<div class="skill">3Cs Design</div>
 		<div class="skill">Game Feel</div>
 		<div class="skill">Gameplay/Systems</div>
 		<div class="skill">UX Design</div>
 	</div>
 	<div class="profile-column">
+		<h3>I am proficient with:</h3>
 		<div class="skill">Unity / Unreal Engine</div>
 		<div class="skill">Programming / Scripting</div>
 		<div class="skill">Confluence / Wikis / Git</div>
 		<div class="skill">Illustrator / Figma</div>
 	</div>
-	<div class="profile-column">
-		<button class="profile-button">Back to Top</button>
-		<button class="profile-button">About</button>
-		<button class="profile-button">Resumé</button>
+	<div class="profile-column buttons">
+		<Button link="#header">Back to Top</Button>
+		<Button link="/about">About</Button>
+		<Button link="/resume">Resume</Button>
 	</div>
 </div>
+
+<style>
+	#columns {
+		width: 100%;
+		max-width: 90rem;
+		display: flex;
+		justify-content: space-between;
+		font-size: 1.5rem;
+		line-height: 200%;
+	}
+	.profile-column {
+		display: flex;
+		flex-direction: column;
+	}
+	h3 {
+		font-size: 1.5rem;
+		margin-bottom: 0.5rem;
+	}
+	.buttons {
+		justify-content: space-between;
+	}
+</style>
---
+++ portfolio/src/lib/hero.svelte
@@ -0,0 +1,40 @@
+<main>
+	<div id="personal">
+		<div id="introduction">
+			<p>Hey there 👋,</p>
+			<p>
+				I'm a driven and ambitious game design graduate from Berlin looking for an
+				<b>entry-level game design</b> position.
+			</p>
+			<p>I am especially intersted in 3Cs and systems.</p>
+			<p>
+				During a 6-month internship at KING Art Games, I was responsible for prototyping and
+				iterating the Combat System and UX of a turn-based tactics game in pre-production.
+			</p>
+		</div>
+		<img src="images/YoshuaPortrait_New_Blob-p-800.png" alt="" />
+	</div>
+</main>
+
+<style>
+	main {
+		max-width: 90rem;
+	}
+	#personal {
+		display: flex;
+		align-items: center;
+	}
+	#introduction {
+		font-size: 2rem;
+		font-weight: 300;
+		line-height: 150%;
+	}
+	p {
+		margin-bottom: 0.8rem;
+	}
+	img {
+		position: relative;
+		left: 9rem;
+		width: 35rem;
+	}
+</style>
