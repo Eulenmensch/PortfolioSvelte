9e19ce210f8f09db908a72cc04f371f8d381bd95
Yoshua Woo <yoshua.woo@gmail.com>
2022-06-07 20:53:33 +0200

    created additional work section

---
+++ portfolio/src/lib/additionalWork.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	import Spacer from './spacer.svelte';
+	import { mediaType } from '../types';
+	import { element } from 'svelte/internal';
+
+	export let data = {
+		mediaFormat: mediaType.image,
+		mediaURL: '',
+		mediaAlt: '',
+		backgroundURL: '',
+		colorOverlay: '',
+		title: '',
+		platform: '',
+		engine: '',
+		genre: '',
+		text: ['']
+	};
+</script>
+
+<div
+	id="project-container"
+	style="background-image:url({data.backgroundURL}); backround-position:center; background-size:cover;"
+>
+	<div style=" height:100%; background-color: {data.colorOverlay};">
+		<div id="media-container">
+			{#if data.mediaFormat == mediaType.image}
+				<img src={data.mediaURL} alt={data.mediaAlt} style="height: 100%; object-fit:contain" />
+			{:else if data.mediaFormat == mediaType.video}
+				<video autoplay loop muted src={data.mediaURL} style="height: 100%;" />
+			{:else if data.mediaFormat == mediaType.iframe}
+				<iframe
+					src={data.mediaURL}
+					title={data.mediaAlt}
+					frameborder="0"
+					width="100%"
+					height="100%"
+					onload="this.style.display = 'inline'"
+				/>
+			{/if}
+		</div>
+		<div id="text-container">
+			<div id="title-container">
+				<h1>{data.title}</h1>
+				<h3>({data.platform})</h3>
+			</div>
+			<h2>{data.genre}</h2>
+			<h4>{data.engine}</h4>
+			<Spacer height="3rem" />
+			{#each data.text as paragraph}
+				<p>{paragraph}</p>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	:root {
+		--text-x-padding: 5rem;
+	}
+	#project-container {
+		max-width: 68rem;
+		border-radius: 2rem;
+		overflow: hidden;
+	}
+	#media-container {
+		aspect-ratio: 16/9;
+	}
+	#text-container {
+		position: relative;
+		padding: 3rem var(--text-x-padding) 4rem;
+	}
+	#title-container {
+		display: flex;
+	}
+	h1 {
+		margin-left: -0.2rem;
+		font-size: 4rem;
+		line-height: 150%;
+	}
+	h2 {
+		font-size: 1.8rem;
+		font-weight: 500;
+	}
+	h3 {
+		font-size: 2.5rem;
+		font-weight: 200;
+		line-height: 150%;
+		padding-top: 0.4rem;
+		padding-left: 0.3rem;
+	}
+	h4 {
+		position: absolute;
+		top: 4.5rem;
+		right: var(--text-x-padding);
+		font-size: 2rem;
+		font-weight: 900;
+	}
+	p {
+		font-size: 1.5rem;
+		line-height: 150%;
+		padding-bottom: 1rem;
+	}
+	iframe {
+		display: none;
+	}
+</style>
---
+++ portfolio/src/lib/additionalWorks.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+	import AdditionalWork from './additionalWork.svelte';
+	import Spacer from './spacer.svelte';
+	import { mediaType } from '../types';
+
+	let additionalWorkDataSet = [
+		{
+			mediaFormat: mediaType.video,
+			mediaURL:
+				'https://uploads-ssl.webflow.com/60cf32d782c9a8cd144e24e5/614a2daaf255a3826c717621_Lightbending%20Cut-transcode.mp4',
+			mediaAlt: '',
+			backgroundURL: 'images/home/lightbendingBackground.png',
+			colorOverlay: 'rgba(5, 4, 26, 0.82)',
+			title: 'Lightbending',
+			platform: 'PC',
+			engine: 'Unity',
+			genre: 'Interactive Installation',
+			text: [
+				'This installation was designed, developed and realized in three weeks.',
+				'By moving adhesive VR trackers to any position on the walls, players can influence the flow of particles that are being projected onto the walls and the ground.',
+				'Two other players can use VR controllers like paint guns to color in the particles flowing through the space.'
+			]
+		},
+		{
+			mediaFormat: mediaType.iframe,
+			mediaURL: 'https://itch.io/embed-upload/1438901?color=f6eed8',
+			mediaAlt: '',
+			backgroundURL: 'images/home/tinypumpBackground.png',
+			colorOverlay: 'rgba(3, 6, 32, 0.86)',
+			title: 'Tiny Pump',
+			platform: 'Web',
+			engine: 'Unity',
+			genre: 'Arcade / Food-Slicing',
+			text: [
+				'A playable banner advertisement for a fictional cup-ramen product.',
+				'Players use the sharp end of a physically simulated noodle to slice the ingredients that then fall into the cup.',
+				"The game uses Unity's DOTS framework, builds to asm.js as well as WebAssembly and can be played in messengers, ad banners or as a conventional web game (Or in the box to the top)."
+			]
+		},
+		{
+			mediaFormat: mediaType.video,
+			mediaURL: 'videos/toytiedGameplay.mp4',
+			mediaAlt: '',
+			backgroundURL: 'images/home/toytiedIllustration.png',
+			colorOverlay: 'rgba(34, 24, 27, 0.9)',
+			title: 'Toy-Tied',
+			platform: 'PC',
+			engine: 'Unity',
+			genre: 'Puzzle / Alternative Controls',
+			text: [
+				'A two-day gamejam game about two plushies tied together with a chinese finger trap.',
+				'The two characters take turns and are moved by hitting the corresponding key on the keyboard, as long as that key is not occupied and also reachable.',
+				'While players can figure out earlier levels in peace, later levels have toy soldiers that fire rockets at the characters.'
+			]
+		},
+		{
+			mediaFormat: mediaType.image,
+			mediaURL: 'images/home/parasiteScreenshot.png',
+			mediaAlt: 'a stylized depiction of a parasitic worm feeding on a cell',
+			backgroundURL: 'images/home/parasiteBackground.png',
+			colorOverlay: 'rgba(19, 6, 14, 0.85)',
+			title: 'Parasite',
+			platform: 'Mobile/Web',
+			engine: 'Unity',
+			genre: 'Farming / Experimental',
+			text: [
+				'A three-day gamejam game where players collaborate to survive as a parasitic organism.',
+				"Once the parasite's hunger has been satisfied, a player can give on their game-code to another player to 'reproduce' and continue the chain of parasites.",
+				'The state of the host body including available resources are given along as well, forcing players to think ahead to leave a good game state for the next player in line.'
+			]
+		}
+	];
+</script>
+
+<h1>Other projects I have worked on:</h1>
+<Spacer height="4rem" />
+<div id="container">
+	{#each additionalWorkDataSet as data, i}
+		<AdditionalWork {data} />
+	{/each}
+</div>
+
+<style>
+	h1 {
+		font-size: 3rem;
+	}
+	#container {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		align-items: stretch;
+		justify-content: space-between;
+		max-width: 140rem;
+	}
+	#container > :global(div) {
+		margin-top: 4rem;
+		flex-grow: 1;
+	}
+</style>
