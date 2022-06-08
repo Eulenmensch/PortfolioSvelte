7b493392f210978120889d749c7f1e45c1ac398c
Yoshua Woo <yoshua.woo@gmail.com>
2022-06-08 22:55:07 +0200

    created project route with property

---
+++ portfolio/src/routes/projects/[project].svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ProjectHeader from '$lib/projectHeader.svelte';
+	import ProjectOverview from '$lib/projectOverview.svelte';
+	import { projectData } from '../../stores/projectStore';
+
+	const projectId = $page.params.project;
+
+	let data: {
+		title: '';
+		titleFont: '';
+		titleSize: '';
+		titleWeight: '';
+		subtitle: '';
+		backgroundImg: '';
+		overlayColorHSLA: '';
+	} = $projectData[projectId];
+</script>
+
+<svelte:head>
+	<style>
+		body {
+			color: whitesmoke;
+		}
+	</style>
+</svelte:head>
+
+<ProjectHeader {data} />
+<ProjectOverview {data} />
