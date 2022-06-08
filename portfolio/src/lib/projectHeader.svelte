07bdf64fa0f1281c113beb7220ef2aff8fe79277
Yoshua Woo <yoshua.woo@gmail.com>
2022-06-08 22:54:51 +0200

    created project page components

---
+++ portfolio/src/lib/projectHeader.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Spacer from './spacer.svelte';
+
+	export let data = {
+		title: '',
+		titleFont: '',
+		titleSize: '',
+		titleWeight: '',
+		subtitle: '',
+		backgroundImg: '',
+		overlayColorHSLA: ''
+	};
+</script>
+
+<header style="background-image: url('../{data.backgroundImg}');">
+	<div id="overlay" style="background-color: hsla({data.overlayColorHSLA})" />
+	<h1
+		style="font-size: calc({data.titleSize} + 1rem); 
+        font-weight: {data.titleWeight}; font-family: {data.titleFont};"
+	>
+		{data.title}
+	</h1>
+	{#if data.subtitle != '' && data.subtitle != undefined}
+		<h2 style="font-family: {data.titleFont}; font-size: calc({data.titleSize} - 2rem);">
+			{data.subtitle}
+		</h2>
+		<Spacer height="2rem" />
+	{/if}
+</header>
+
+<style>
+	header {
+		position: relative;
+		z-index: 0;
+		height: 20rem;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-size: cover;
+		background-position: center;
+	}
+	#overlay {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+		filter: opacity(80%);
+	}
+	h1 {
+		line-height: 100%;
+		padding-bottom: 1rem;
+	}
+</style>
---
+++ portfolio/src/lib/projectOverview.svelte
@@ -0,0 +1,54 @@
+<script>
+	export let data = {
+		videoId2: '',
+		title: '',
+		facts: {
+			genre: '',
+			engine: '',
+			'team size': '',
+			duration: '',
+			role: ''
+		},
+		gameDescription: '',
+		videoDescription: '',
+		calloutText: ['']
+	};
+</script>
+
+<div id="container">
+	<div id="facts-video-container">
+		<div id="facts-container">
+			<div id="quick-facts">
+				{#each Object.entries(data.facts) as fact}
+					<div id="fact">
+						<div id="fact-key">{fact[0]}</div>
+						<div id="fact-value">{fact[1]}</div>
+					</div>
+				{/each}
+			</div>
+			<div id="game-description"><p>{data.gameDescription}</p></div>
+			<div id="video-description"><p>{data.videoDescription}</p></div>
+		</div>
+		{#if data.videoId2 != '' && data.videoId2 != undefined}
+			<div id="video-container">
+				<iframe
+					width="100%"
+					height="100%"
+					src="https://www.youtube-nocookie.com/embed/{data.videoId2}"
+					title="{data.title} youtube video"
+					frameborder="0"
+					allow="clipboard-write; encrypted-media; picture-in-picture"
+					allowfullscreen
+				/>
+			</div>
+		{/if}
+	</div>
+	<div id="intro-text">
+		{#each data.calloutText as paragraph}
+			<p>{paragraph}</p>
+		{/each}
+	</div>
+</div>
+
+<style>
+</style>
