45ad73e8477d334e76ec9241968e3425fff616a6
Yoshua Woo <yoshua.woo@gmail.com>
2022-07-28 20:05:32 +0200

    project slice component export

---
+++ portfolio/src/lib/projectSlices/index.ts
@@ -0,0 +1,17 @@
+import Header from "./header.svelte";
+import HeroVideo from "./heroVideo.svelte";
+import Facts from "./facts.svelte";
+import ElevatorPitch from "./elevatorPitch.svelte";
+import Responsibilities from "./responsibilities.svelte";
+import ImageParagraph from "./imageParagraph.svelte";
+
+const components = {
+    header: Header,
+    hero_video: HeroVideo,
+    facts: Facts,
+    elevator_pitch: ElevatorPitch,
+    responsibilities: Responsibilities,
+    paragraph_with_image: ImageParagraph
+}
+
+export default components;
