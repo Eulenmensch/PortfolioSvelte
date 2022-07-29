212c665f5bb5a0341242339cbd147870930ef2da
Yoshua Woo <yoshua.woo@gmail.com>
2022-07-29 20:06:29 +0200

    hero video component

---
+++ portfolio/src/lib/projectSlices/heroVideo.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as prismicH from '@prismicio/helpers';
+	export let slice: { [key: string]: any };
+	export let context: { [key: string]: any };
+</script>
+
+<main>
+	<div>
+		<iframe
+			width="100%"
+			height="100%"
+			src={prismicH.asLink(slice.primary.video_url)}
+			title="{context.doc.uid} youtube video"
+			frameborder="0"
+			allow="clipboard-write; encrypted-media; picture-in-picture"
+			allowfullscreen
+		/>
+	</div>
+</main>
+
+<style>
+	div {
+		width: 100vw;
+		aspect-ratio: 16/9;
+	}
+</style>
