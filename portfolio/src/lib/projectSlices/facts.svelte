6ef8859a87f09a4e13ad826b5e1373115f5402dd
Yoshua Woo <yoshua.woo@gmail.com>
2022-07-29 21:47:10 +0200

    facts slice component

---
+++ portfolio/src/lib/projectSlices/facts.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let slice: { [key: string]: any };
+</script>
+
+{#each Object.entries(slice.primary) as [title, fact]}
+	<div id="fact-container">
+		<!-- since I'm using the field's UID, I have to format it from unique_ID to unique ID -->
+		<h3>{title.replace('_', ' ')}</h3>
+		<p>{fact}</p>
+	</div>
+{/each}
+
+<style>
+	#fact-container {
+		display: flex;
+	}
+	h3 {
+		width: 7rem;
+		font-size: 1rem;
+		text-transform: capitalize;
+	}
+</style>
