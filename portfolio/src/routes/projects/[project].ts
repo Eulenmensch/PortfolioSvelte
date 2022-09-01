cf467c14d59b59f9939e21731730349e1722e176
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-01 21:53:31 +0200

    strongly typed var in project parametric route

--- portfolio/src/routes/projects/[project].ts
+++ portfolio/src/routes/projects/[project].ts
@@ -1,6 +1,6 @@
 import * as prismic from "@prismicio/client"
 
-export async function GET({ params }) {
+export async function GET({ params }: { [key: string]: any }) {
 
     const repositoryName = "yoshua-portfolio";
     const client = prismic.createClient(repositoryName);
