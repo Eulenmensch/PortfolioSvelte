38845ce00baced54e335c894c9ce9532106c00b2
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-02 14:25:16 +0200

    headshot component with animated blob

---
+++ portfolio/src/lib/home/headshot.svelte
@@ -0,0 +1,97 @@
+<script>
+	import { spline } from '@georgedoescode/spline';
+	import * as simplex from 'simplex-noise';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const path = document.querySelector('path');
+		function createPoints() {
+			const points = [];
+			const numPoints = 6;
+			const angleStep = (Math.PI * 2) / numPoints;
+			const radius = 80;
+
+			for (let i = 1; i <= numPoints; i++) {
+				const theta = i * angleStep;
+				const x = 100 + Math.cos(theta) * radius;
+				const y = 100 + Math.sin(theta) * radius;
+
+				points.push({
+					x: x,
+					y: y,
+					initialX: x,
+					initialY: y,
+					noiseOffsetX: Math.random() * 100,
+					noiseOffsetY: Math.random() * 100
+				});
+			}
+
+			return points;
+		}
+
+		const points = createPoints();
+
+		function map(n, start1, end1, start2, end2) {
+			return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+		}
+
+		const simplexNoise = simplex.createNoise2D();
+
+		let noiseStep = 0.0005;
+		function noise(x, y) {
+			return simplexNoise(x, y);
+		}
+
+		(function animate() {
+			path.setAttribute('d', spline(points, 1, true));
+			requestAnimationFrame(animate);
+
+			for (let i = 0; i < points.length; i++) {
+				const point = points[i];
+				const nX = noise(point.noiseOffsetX, point.noiseOffsetX);
+				const nY = noise(point.noiseOffsetY, point.noiseOffsetY);
+				const x = map(nX, -1, 1, point.initialX - 20, point.initialX + 20);
+				const y = map(nY, -1, 1, point.initialY - 20, point.initialY + 20);
+
+				point.x = x;
+				point.y = y;
+
+				point.noiseOffsetX += noiseStep;
+				point.noiseOffsetY += noiseStep;
+			}
+		})();
+	});
+</script>
+
+<main>
+	<svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+		<defs>
+			<!-- Our gradient fill #gradient -->
+			<pattern id="portrait" patternUnits="userSpaceOnUse" width="200" height="200">
+				<image href="images/home/YoshuaPortraitHalftone.jpg" x="0" y="0" width="200" height="200" />
+			</pattern>
+		</defs>
+		<path d="" fill="url(#portrait)" stroke="#FFBB25" stroke-width="6" />
+	</svg>
+</main>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	main {
+		margin-top: 13rem;
+		height: 100%;
+		width: 100vw;
+		display: grid;
+		place-items: center;
+	}
+
+	svg {
+		width: 90vmin;
+		height: 90vmin;
+	}
+</style>
