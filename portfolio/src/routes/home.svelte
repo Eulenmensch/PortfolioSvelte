754c77de7997e4bb0b6557d6bda8fc5b2e5394e3
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-02 22:19:24 +0200

    home route prismic data fetching

--- portfolio/src/routes/home.svelte
+++ portfolio/src/routes/home.svelte
@@ -2,14 +2,35 @@
 	import { Body } from 'svelte-body';
 	import Headshot from '$lib/home/headshot.svelte';
 	import Wave from '$lib/home/wave.svelte';
+	import ProjectCard from '$lib/home/projectCard.svelte';
 	const style = {
 		backgroundColor: '#636A25',
 		width: '100vw'
 	};
+
+	export let document: { [key: string]: any };
+
+	// Colors from: https://hue.tools/mix?mode=lch&colors=636a25ff%3B7600ffff&gamma=1.01&format=hex
+	// FIXME: Should be solved using an algorithm instead of repeating the colors
+	let colors = [
+		'#FFBB25',
+		'#B53401',
+		'#3F62A4',
+		'#FFBB25',
+		'#B53401',
+		'#3F62A4',
+		'#FFBB25',
+		'#B53401',
+		'#3F62A4'
+	];
 </script>
 
 <Body {style} />
 <Headshot />
 <Wave />
+
+{#each Object.entries(document) as [key, project], i}
+	<ProjectCard bind:project bind:color={colors[i]} />
+{/each}
 
 <style></style>
---
+++ portfolio/src/routes/home.ts
@@ -0,0 +1,18 @@
+import * as prismic from "@prismicio/client"
+
+export async function GET({ params }: { [key: string]: any }) {
+
+    const repositoryName = "yoshua-portfolio";
+    const client = prismic.createClient(repositoryName);
+    const document = await client.getAllByType('project_page', params.project);
+
+    if (document) {
+        return {
+            body: { document }
+        };
+    }
+
+    return {
+        status: 404
+    };
+}
