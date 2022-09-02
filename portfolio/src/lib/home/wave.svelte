39681c2a80576cd38c08f155277b07e3cd952471
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-02 14:26:40 +0200

    wave component

---
+++ portfolio/src/lib/home/wave.svelte
@@ -0,0 +1,21 @@
+<div>
+	<img src="images/home/hero-wave.svg" alt="" />
+</div>
+
+<style>
+	div {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 70rem;
+		overflow-x: hidden;
+	}
+	img {
+		position: absolute;
+		top: -8rem;
+		left: -42rem;
+		z-index: -10;
+		height: 70rem;
+	}
+</style>
