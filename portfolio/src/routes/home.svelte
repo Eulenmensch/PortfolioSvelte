fee374f93d068ca6b84446ea392d416628c60ee6
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-02 14:26:27 +0200

    temp home route to later replace index

---
+++ portfolio/src/routes/home.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Body } from 'svelte-body';
+	import Headshot from '$lib/home/headshot.svelte';
+	import Wave from '$lib/home/wave.svelte';
+	const style = {
+		backgroundColor: '#636A25',
+		width: '100vw'
+	};
+</script>
+
+<Body {style} />
+<Headshot />
+<Wave />
+
+<style></style>
