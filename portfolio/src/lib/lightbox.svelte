f8879d503e2352b4d370a768c81604990d904698
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-10 19:00:25 +0200

    Secondary video and lightbox components

---
+++ portfolio/src/lib/lightbox.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	export let url: string | null = '';
+
+	let innerWidth: number;
+</script>
+
+<svelte:window bind:innerWidth />
+
+<main on:click class:widescreen={innerWidth > 1023}>
+	<div>
+		<img src={url} alt="" />
+		<slot>image missing</slot>
+	</div>
+</main>
+
+<style>
+	main {
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: 100vh;
+		width: 100vw;
+		background: rgba(0, 0, 0, 0.9);
+		z-index: 1000;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	div {
+		display: flex;
+		max-width: 90vw;
+		/* height: 50vh; */
+		flex-direction: column;
+		align-items: center;
+		flex-wrap: wrap;
+	}
+	img {
+		/* height: 100%; */
+		max-width: 100%;
+		/* margin: auto; */
+		max-height: 90vh;
+		border-radius: var(--border-radius);
+		background-size: cover;
+		background-position: 50%;
+	}
+	.widescreen div :global(p) {
+		margin-top: 2rem;
+		width: 60%;
+		text-align: center;
+	}
+</style>
---
+++ portfolio/src/lib/projectSlices/secondaryVideo.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	export let slice: { [key: string]: any };
+	export let context: { [key: string]: any };
+
+	let innerWidth: number;
+</script>
+
+<svelte:window bind:innerWidth />
+
+<main class:widescreen={innerWidth > 1023}>
+	<div>
+		{@html slice.primary.video_url.html}
+	</div>
+</main>
+
+<style>
+	div {
+		display: flex;
+		align-items: stretch;
+		justify-content: stretch;
+		width: 100%;
+		max-width: 90rem;
+		margin: 2rem auto;
+		padding: 0 var(--x-padding);
+	}
+	.widescreen > div {
+		margin: 0;
+		padding: 0;
+	}
+	div > :global(iframe) {
+		width: 100%;
+		height: 100%;
+		aspect-ratio: 16/9;
+		border-radius: var(--border-radius);
+		overflow: hidden;
+		box-shadow: var(--box-shadow);
+	}
+</style>
