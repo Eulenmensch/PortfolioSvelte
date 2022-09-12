5a0780562a9aecad71defb8ce0ba0c162c608a8c
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-12 20:07:03 +0200

    contact form and jquery package

--- portfolio/src/lib/home/contactForm.svelte
+++ portfolio/src/lib/home/contactForm.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import Button from '$lib/button.svelte';
 	import * as prismicH from '@prismicio/helpers';
+	import jQuery from 'jquery';
 	import { onMount } from 'svelte';
+	import ContactInfo from './contactInfo.svelte';
+	import { slide } from 'svelte/transition';
 
 	export let contactSlice: { [key: string]: any };
 
@@ -13,6 +17,19 @@
 	});
 
 	let innerWidth: number;
+
+	let successfulSubmission: boolean = false;
+
+	function submitForm() {
+		jQuery.ajax({
+			url: 'https://api.staticforms.xyz/submit',
+			type: 'post',
+			data: jQuery('#contact-form').serialize(),
+			success: function () {
+				successfulSubmission = true;
+			}
+		});
+	}
 </script>
 
 <svelte:window bind:innerWidth />
@@ -20,6 +37,7 @@
 <main
 	class:tablet={innerWidth > 767}
 	class:widescreen={innerWidth > 1023}
+	class:successfulSubmission
 	style:box-shadow={shadow}
 	style:--background-image={'url(' +
 		prismicH.asImageSrc(contactSlice.primary.background_image) +
@@ -28,61 +46,20 @@
 	<div id="cta">
 		<h1>{prismicH.asText(contactSlice.primary.call_to_action)}</h1>
 		{#if innerWidth > 767}
-			<div id="contact-info-container">
-				<div class="contact-info">
-					<svg
-						stroke-width="2.5"
-						viewBox="0 0 24 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						color="currentColor"
-						><path
-							d="M7 9l5 3.5L17 9"
-							stroke="currentColor"
-							stroke-width="2.5"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/><path
-							d="M2 17V7a2 2 0 012-2h16a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2z"
-							stroke="currentColor"
-							stroke-width="2.5"
-						/>
-					</svg>
-					<a href={'mailto:' + contactSlice.primary.email}>{contactSlice.primary.email}</a>
-				</div>
-				<div class="contact-info">
-					<svg
-						stroke-width="2.5"
-						viewBox="0 0 24 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						color="currentColor"
-						><path
-							d="M18.118 14.702L14 15.5c-2.782-1.396-4.5-3-5.5-5.5l.77-4.13L7.815 2H4.064c-1.128 0-2.016.932-1.847 2.047.42 2.783 1.66 7.83 5.283 11.453 3.805 3.805 9.286 5.456 12.302 6.113 1.165.253 2.198-.655 2.198-1.848v-3.584l-3.882-1.479z"
-							stroke="currentColor"
-							stroke-width="2.5"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/></svg
-					>
-					<a href={'tel:' + contactSlice.primary.telephone_number}
-						>{contactSlice.primary.telephone_number}</a
-					>
-				</div>
-			</div>
+			<ContactInfo bind:contactSlice />
 		{/if}
 	</div>
-	<form action="https://api.staticforms.xyz/submit" method="post">
-		<input type="text" name="name" placeholder="Your Name" />
-		<input type="text" name="email" placeholder="Your Email" />
-		<textarea name="message" placeholder="Your Message..." />
+	<form id="contact-form" class:successfulSubmission>
+		<input type="text" name="name" placeholder="Your Name" required />
+		<input type="text" name="email" placeholder="Your Email" required />
+		<textarea name="message" placeholder="Your Message..." required />
 		<input type="text" name="honeypot" style="display:none" />
 		<input type="hidden" name="accessKey" value="c184eab0-febe-4343-9048-5366fadada04" />
 		<input type="hidden" name="subject" value="Portfolio Inquiry" />
 		<input type="hidden" name="replyTo" value="@" />
 		<!-- <input type="hidden" name="redirectTo" value="https://example.com/contact/success" /> -->
-		<div id="submit">
-			<input type="submit" value="Submit" />
+		<div id="submit" on:click={submitForm}>
+			<!-- <input type="submit" value="Submit" />
 			<svg
 				viewBox="0 0 24 24"
 				stroke-width="2.5"
@@ -101,50 +78,36 @@
 					stroke-width="2.5"
 					stroke-linecap="round"
 				/>
-			</svg>
-		</div>
-	</form>
-	{#if innerWidth < 768}
-		<div id="contact-info-container">
-			<div class="contact-info">
+			</svg> -->
+			<Button color="white">
+				Submit
 				<svg
-					stroke-width="2.5"
 					viewBox="0 0 24 24"
+					stroke-width="2.5"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
-					color="#1a1a1a"
+					color="currentColor"
 					><path
-						d="M7 9l5 3.5L17 9"
-						stroke="#1a1a1a"
+						d="M9 9l4.5 3L18 9M3 13.5h2M1 10.5h4"
+						stroke="currentColor"
 						stroke-width="2.5"
 						stroke-linecap="round"
 						stroke-linejoin="round"
 					/><path
-						d="M2 17V7a2 2 0 012-2h16a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2z"
-						stroke="#1a1a1a"
-						stroke-width="2.5"
-					/>
-				</svg>
-				<a href="mailto:yoshua.woo@gmail.com">yoshua.woo@gmail.com</a>
-			</div>
-			<div class="contact-info">
-				<svg
-					stroke-width="2.5"
-					viewBox="0 0 24 24"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-					color="#1a1a1a"
-					><path
-						d="M18.118 14.702L14 15.5c-2.782-1.396-4.5-3-5.5-5.5l.77-4.13L7.815 2H4.064c-1.128 0-2.016.932-1.847 2.047.42 2.783 1.66 7.83 5.283 11.453 3.805 3.805 9.286 5.456 12.302 6.113 1.165.253 2.198-.655 2.198-1.848v-3.584l-3.882-1.479z"
-						stroke="#1a1a1a"
+						d="M5 7.5V7a2 2 0 012-2h13a2 2 0 012 2v10a2 2 0 01-2 2H7a2 2 0 01-2-2v-.5"
+						stroke="currentColor"
 						stroke-width="2.5"
 						stroke-linecap="round"
-						stroke-linejoin="round"
-					/></svg
-				>
-				<a href="tel:+4915754381194">+49 1575 438 1194</a>
-			</div>
+					/>
+				</svg>
+			</Button>
 		</div>
+		{#if successfulSubmission}
+			<div transition:slide={{ duration: 300 }} id="submission-success"><div>Thank you!</div></div>
+		{/if}
+	</form>
+	{#if innerWidth < 768}
+		<ContactInfo bind:contactSlice />
 	{/if}
 </main>
 
@@ -214,6 +177,10 @@
 		padding: 3rem 4rem;
 	}
 
+	.successfulSubmission {
+		background-color: var(--kaomaru-blue);
+	}
+
 	input[type='text'],
 	#submit {
 		padding: 1rem;
@@ -233,19 +200,32 @@
 	}
 
 	#submit {
+		padding: 0;
 		margin-top: 1rem;
 		align-self: flex-end;
 		display: flex;
+		border: none;
+	}
+	.widescreen #submit {
+		color: var(--text-color-primary);
+		padding: 0;
 	}
 	#submit svg {
+		margin-left: 0.5rem;
 		height: 2rem;
 		width: 2rem;
 	}
+	/* #submit :global(*) {
+		border: unset;
+	}
+	.widescreen #submit :global(*) {
+		border: initial;
+	} */
 
-	input[type='submit'] {
+	/* input[type='submit'] {
 		all: unset;
 		margin: 0 0.5rem;
-	}
+	} */
 	input::placeholder {
 		color: #1a1a1a;
 	}
@@ -266,31 +246,25 @@
 		padding-left: 1.5rem;
 	}
 
-	#contact-info-container {
-		margin-top: 2.5rem;
-		margin-bottom: 1.5rem;
-	}
-	.tablet #contact-info-container {
-		margin-top: 0.5rem;
-	}
-
-	.contact-info {
-		margin: 0.5rem 0;
+	#submission-success {
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		width: 100%;
+		height: 100%;
 		display: flex;
 		align-items: center;
-		font-size: 1.5rem;
-		font-weight: 900;
+		justify-content: center;
+		background-color: var(--kaomaru-blue);
 	}
-	.contact-info svg {
-		width: 3rem;
-		aspect-ratio: 1/1;
-	}
-	.contact-info a {
-		color: #1a1a1a;
-		margin-left: 0.5rem;
+	.tablet #submission-success {
+		left: 50%;
+		width: 50%;
 	}
 
-	.tablet .contact-info a {
-		color: var(--text-color-primary);
+	#submission-success div {
+		font-size: 4rem;
+		font-weight: 900;
+		margin-bottom: 2rem;
 	}
 </style>
---
+++ portfolio/src/lib/home/contactInfo.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	export let contactSlice: { [key: string]: any };
+	let innerWidth: number;
+</script>
+
+<svelte:window bind:innerWidth />
+
+<main class:tablet={innerWidth > 767}>
+	<div id="contact-info-container">
+		<div class="contact-info">
+			<svg
+				stroke-width="2.5"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				color="currentColor"
+				><path
+					d="M7 9l5 3.5L17 9"
+					stroke="currentColor"
+					stroke-width="2.5"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/><path
+					d="M2 17V7a2 2 0 012-2h16a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2z"
+					stroke="currentColor"
+					stroke-width="2.5"
+				/>
+			</svg>
+			<a href={'mailto:' + contactSlice.primary.email}>{contactSlice.primary.email}</a>
+		</div>
+		<div class="contact-info">
+			<svg
+				stroke-width="2.5"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				color="currentColor"
+				><path
+					d="M18.118 14.702L14 15.5c-2.782-1.396-4.5-3-5.5-5.5l.77-4.13L7.815 2H4.064c-1.128 0-2.016.932-1.847 2.047.42 2.783 1.66 7.83 5.283 11.453 3.805 3.805 9.286 5.456 12.302 6.113 1.165.253 2.198-.655 2.198-1.848v-3.584l-3.882-1.479z"
+					stroke="currentColor"
+					stroke-width="2.5"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/></svg
+			>
+			<a href={'tel:' + contactSlice.primary.telephone_number}
+				>{contactSlice.primary.telephone_number}</a
+			>
+		</div>
+		<div class="contact-info">
+			<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+				><title>LinkedIn</title>
+				<path
+					fill="currentColor"
+					d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
+				/>
+			</svg>
+			<a target="_blank" rel="noopener noreferrer" href={contactSlice.primary.linkedin}>LinkedIn®</a
+			>
+		</div>
+	</div>
+</main>
+
+<style>
+	#contact-info-container {
+		margin-top: 2.5rem;
+		margin-bottom: 1.5rem;
+	}
+	.tablet #contact-info-container {
+		margin-top: 0.5rem;
+	}
+
+	.contact-info {
+		margin: 0.5rem 0;
+		display: flex;
+		align-items: center;
+		font-size: 1.5rem;
+		font-weight: 900;
+	}
+	.tablet .contact-info {
+		padding: 0.3rem 0;
+	}
+
+	.contact-info svg {
+		width: 3rem;
+		aspect-ratio: 1/1;
+		color: #1a1a1a;
+	}
+	.tablet .contact-info svg {
+		color: var(--text-color-primary);
+	}
+
+	.contact-info a {
+		color: #1a1a1a;
+		margin-left: 0.5rem;
+	}
+
+	.tablet .contact-info a {
+		color: var(--text-color-primary);
+		margin-left: 1rem;
+	}
+</style>
