0661146e0323efc97f4207ed2f8e6cc1db224dd2
Yoshua Woo <yoshua.woo@gmail.com>
2022-09-14 19:24:39 +0200

    Updated resume page styling

--- portfolio/src/routes/resume.svelte
+++ portfolio/src/routes/resume.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
 	import * as prismicH from '@prismicio/helpers';
-	import Spacer from '$lib/spacer.svelte';
+	import { Body } from 'svelte-body';
 
 	export let document: { [key: string]: any };
 	let page = document.results[0].data;
 	console.log(page.link.url);
 
 	let innerWidth: number;
+
+	let style: { background: string; color: string };
+	$: if (innerWidth < 768) {
+		style = {
+			background: 'linear-gradient(hsl(218, 91%, 21%), hsl(218, 91%, 10%))',
+			color: 'whitesmoke'
+		};
+	}
+	$: if (innerWidth > 767) {
+		style = {
+			background: 'linear-gradient(hsl(218, 91%, 21%), hsl(218, 91%, 10%) 20%)',
+			color: 'whitesmoke'
+		};
+	}
 </script>
 
 <svelte:window bind:innerWidth />
 
 <svelte:head>
-	<style>
-		body {
-			background-color: hsla(218, 91%, 21%, 1);
-			color: whitesmoke;
-		}
-	</style>
 	<title>Resumé</title>
 </svelte:head>
 
-<Spacer height="5rem" />
-<main>
+<Body {style} />
+
+<main class:tablet={innerWidth > 767} class:widescreen={innerWidth > 1023}>
 	<h1>Resumé</h1>
-	<Spacer height="10rem" />
-	{#if innerWidth >= 768}
+	{#if innerWidth > 767}
 		<div id="resume-container">
 			<div id="resume-embed">
 				<iframe
@@ -55,9 +63,13 @@
 		width: 100%;
 	}
 	h1 {
+		margin: 5rem 0 10rem;
 		font-size: 5rem;
 		line-height: 100%;
 		text-shadow: 0.2rem 0.2rem blue;
+	}
+	.tablet h1 {
+		margin: 2rem 0 5.5rem;
 	}
 	#resume-container {
 		display: flex;
@@ -68,9 +80,16 @@
 	}
 	#resume-embed {
 		width: 100%;
-		max-width: 100rem;
-		aspect-ratio: 1/1.2;
+		max-width: 80rem;
+		aspect-ratio: 1/1.4142;
+		max-height: 100rem;
 	}
+	.widescreen #resume-embed {
+		border-radius: var(--border-radius);
+		overflow: hidden;
+		box-shadow: var(--box-shadow);
+	}
+
 	iframe {
 		border-style: none;
 	}
